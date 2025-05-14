951c5d481d7a462b71f4898dfd228d9d6e3afdc1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-14 12:48:07 +0530

    reactlibrary: RNSplashyPackage

--- android/src/main/java/com/reactlibrary/RNSplashyPackage.java
+++ android/src/main/java/com/reactlibrary/RNSplashyPackage.java
@@ -1,28 +1,32 @@
 
 package com.reactlibrary;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class RNSplashyPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNSplashyModule(reactContext));
-    }
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
+public class SplashScreenPackage implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new SplashScreenModule(reactContext));
+
+        return modules;
     }
 }
