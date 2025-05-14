6fedefa10a5c10203260e41008b1340c15400130
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-14 12:48:41 +0530

    ios: RNSplashy

--- ios/RNSplashy.h
+++ ios/RNSplashy.h
@@ -1,11 +1,9 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
+#import <React/RCTRootView.h>
 
-@interface RNSplashy : NSObject <RCTBridgeModule>
+@interface RCTSplashScreen : NSObject <RCTBridgeModule>
+
++ (NSString *)splashImageNameForOrientation;
++ (void)show:(RCTRootView *)v;
 
 @end
-  
